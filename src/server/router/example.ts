4f5f728c1f53dec6e3bce6438fa2a8d4164384ca
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-09-13 21:03:11 -0400

    wip

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -18,4 +18,29 @@ export const exampleRouter = createRouter()
     async resolve({ ctx }) {
       return await ctx.prisma.example.findMany();
     },
+  }).mutation("create", {
+    input: z.object({ text: z.string() }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.example.create({
+        data: { text: input.text },
+      });
+    },
+  })
+  .mutation("update", {
+    input: z.object({ id: z.string(), text: z.string() }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.example.update({
+        where: { id: input.id },
+        data: { text: input.text },
+      });
+    },
+  })
+  .mutation("delete", {
+    input: z.object({ id: z.string() }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.example.delete({
+        where: { id: input.id },
+      });
+    },
   });
+
