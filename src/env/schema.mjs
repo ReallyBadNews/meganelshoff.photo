79b722a7b45812f550f72d1d324018978d257db8
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-10-09 10:19:55 -0400

    fix: `NEXTAUTH_URL`

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -9,10 +9,13 @@ export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL:
-    process.env.VERCEL_ENV === "production"
-      ? z.string().url().optional()
-      : z.string().url(),
+  NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesnt include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url()
+  ),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   CLOUDINARY_CLOUD_NAME: z.string(),
