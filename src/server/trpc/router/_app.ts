b51e03296e2efdc5571892ad98e905f7e71de256
Kenny Elshoff <kelshoff@grahamdigital.com>
2022-10-15 21:49:58 -0400

    replace example with cloudinary router and add getById procedure

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,10 @@
-// src/server/trpc/router/index.ts
+// src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
+import { cloudinaryRouter } from "./cloudinary";
 
 export const appRouter = router({
-  example: exampleRouter,
+  cloudinary: cloudinaryRouter,
   auth: authRouter,
 });
 
